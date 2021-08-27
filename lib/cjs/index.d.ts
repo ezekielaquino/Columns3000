3e47afefce6816af7f1678c2ef6e5d546ac8dffd
Ezekiel Aquino <ezekielaquino@users.noreply.github.com>
2021-08-27 14:47:01 +0200

    Updates lib

--- lib/cjs/index.d.ts
+++ lib/cjs/index.d.ts
@@ -4,12 +4,14 @@ declare type ColumnsBase = {
     justify: React.CSSProperties['justifyContent'];
     gutter: string;
     breakpoint: string;
+    className?: string;
 };
 export declare type ColumnsType = Partial<ColumnsBase>;
 export declare type ColumnType = {
     span: number;
     grow?: boolean;
     breakpoint?: string;
+    className?: string;
 };
 export declare const Columns: React.FC<ColumnsType>;
 export declare const Column: React.FC<ColumnType>;
